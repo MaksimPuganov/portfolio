4f7c86c7585df01dfab09de2e2615b2c74ac10e5
Maksim Puganov <aquariustech@yandex.ru>
2015-10-05 08:19:29 +0300

    Дополнительные расширения gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,33 @@
-var gulp = require("gulp"),
-    browserSync = require('browser-sync');
+"use strict";
 
-// Запуск сервера
+var gulp = require("gulp"),
+    browserSync = require('browser-sync'),
+    spritesmith = require('gulp.spritesmith'),
+    concatCss = require('gulp-concat-css'),
+    autoprefixer = require('gulp-autoprefixer');
+
+//Генерация спрайтов из картинок
+
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('app/img/icon/*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: 'sprite.css'
+  }));
+  return spriteData.pipe(gulp.dest('app/sprites/'));
+});
+
+
+gulp.task('default', function () {
+    return gulp.src('app/css/**/*.css')
+        .pipe(concatCss("app/css/common.css"))
+        .pipe(autoprefixer({
+            browsers: ['last 15 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/css/'));
+});
+
+// Запуск сервера Browser-sync
 gulp.task('server', function () {
     browserSync({
         port: 9000,
