8573b8addc4520db9f9cc32683973f8adbb48da7
= <aquariustech@yandex.ru>
2015-10-09 18:37:27 +0300

    Добавление Popup'а

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
     console.log('js запустился');
     console.log($);
+
+    $('.trigger').on('click', function(e) {
+		e.preventDefault();
+
+		$('.popup').bPopup({
+			closeClass : 'popup__close'
+		});
+	})
 })
