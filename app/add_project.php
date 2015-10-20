59c55ed1e4ec1864f9f7f0ff3a17e9a177be90dd
Maksim Puganov <aquariustech@yandex.ru>
2015-10-20 18:47:44 +0300

    Добавлено часть валидации Popup'а

--- app/add_project.php
+++ app/add_project.php
@@ -1,7 +1,15 @@
 <?php 
+	$name = $_POST['nameProject'];
 	$data = array();
 
-	$data['message'] = 'OK';
+
+	if ($name === '') {
+		$data['status'] = 'error';
+		$data['text'] = 'Заполните имя!';
+	}else {
+		$data['status'] = 'OK';
+		$data['text'] = 'Спасибо, что заполнили все поля!';
+	}
 
 	header("Content-Type: application/json");
 	echo json_encode($data);
--- app/js/project.js
+++ app/js/project.js
@@ -14,45 +14,57 @@ var myModule = (function () {
 		console.log('Модальное окно');
 		e.preventDefault();
 
-		$('.popup').bPopup({
-			closeClass : 'popup-close'
+		var divPopup = $('.popup'),
+			form = divPopup.find('.form-popup');
+
+		divPopup.bPopup({
+			closeClass : 'popup-close',
+			onClose : function () {
+				form.find('.message').text('').hide();
+			}
 			});
 		};
 
 	var _addProject = function (e) {
-			console.log('Добавдение проекта');
+			console.log('Добавление проекта');
 			e.preventDefault();
 
 			//объявление переменных	
 			var form = $(this),
 				url = 'add_project.php',
-				data = form.serialize();
-
-			console.log(data);
-
+				serverAnswer = _ajaxProject(form, url);
 			//запрос на сервер 
-			$.ajax({
-				url: url,
-				type: 'POST',
-				dataType: 'json',
-				data: data,
-			})
-			.done(function(answer) {
-				console.log("success");
-				if (answer.message === "OK") {
-					console.log('Все прошло успешно!');
-				};
-			})
-			.fail(function() {
-				console.log("error");
-			})
-			.always(function() {
-				console.log("complete");
-			});
-			
+			serverAnswer.done(function(answer) {
+				var successBlock = form.find('.success-message'),
+					errorBlock = form.find('.error-message');
 
+				if (answer.status === "OK") {
+					errorBlock.hide();
+					successBlock.text(answer.text).show();
+					console.log('Все прошло успешно!');
+				}else {
+					successBlock.hide();
+					errorBlock.text(answer.text).show();
+				};
+			});
 		};
 
+	var _ajaxProject = function (form, url) {
+
+		data = form.serialize();
+
+		var resultAjax = $.ajax({
+			url: url,
+			type: 'POST',
+			dataType: 'json',
+			data: data,
+		}).fail( function (answer) {
+			console.log('Проблемы с PHP');
+			form.find('.error-message').text('на сервере произошла ошибка').show();
+		});
+
+		return resultAjax;
+	};
 	return {
 		init : init
 	}
